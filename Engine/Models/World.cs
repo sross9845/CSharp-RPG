b463dbd0b53271a4607dfea0a271b093e8154305
Sean Ross <sean.ross.985@gmail.com>
2020-01-16 13:30:13 -0800

    added locations

--- Engine/Models/World.cs
+++ Engine/Models/World.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    class World
+    public class World
     {
+        private List<Location> _locations = new List<Location>();
+
+        internal void AddLocation(int xCoordinate, int yCoordinate, string name, string description, string imageName)
+        {
+            Location loc = new Location();
+            loc.XCoordinate = xCoordinate;
+            loc.YCoordinate = yCoordinate;
+            loc.Name = name;
+            loc.Description = description;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc);
+        }
+
+        public Location LocationAt(int xCoordinate, int yCoordinate)
+        {
+            foreach (Location loc in _locations)
+            {
+                if (loc.XCoordinate == xCoordinate && loc.YCoordinate == yCoordinate)
+                {
+                    return loc;
+                }
+            }
+
+            return null;
+        }
     }
 }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,30 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
         public Location CurrentLocation { get; set; }
+
         public GameSession()
         {
             CurrentPlayer = new Player();
-            CurrentPlayer.Name = "Sean";
+            CurrentPlayer.Name = "Scott";
             CurrentPlayer.CharacterClass = "Fighter";
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
 
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your house";
-           CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
     }
 }
-     
